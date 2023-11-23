1f2fe7e246debc05e6ded45166a7d7521e20dc62
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2023-11-23 17:40:17 -0500

    Add Linux Arm64 support attempt 1

--- buildSrc/src/main/groovy/tool/generator/GenerateLibs.groovy
+++ buildSrc/src/main/groovy/tool/generator/GenerateLibs.groovy
@@ -18,6 +18,7 @@ class GenerateLibs extends DefaultTask {
     private final String[] buildEnvs = System.getProperty('envs')?.split(',')
     private final boolean forWindows = buildEnvs?.contains('windows')
     private final boolean forLinux = buildEnvs?.contains('linux')
+    private final boolean forLinuxArm64 = buildEnvs?.contains('linuxarm64')
     private final boolean forMac = buildEnvs?.contains('macos')
     private final boolean forMacArm64 = buildEnvs?.contains('macosarm64')
     private static final boolean isARM = System.getProperty("os.arch").equals("arm") || System.getProperty("os.arch").startsWith("aarch64");
@@ -97,6 +98,12 @@ class GenerateLibs extends DefaultTask {
             buildTargets += linux64
         }
 
+        if (forLinuxArm64) {
+            def linuxArm64 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.Linux, true, true)
+            addFreeTypeIfEnabled(linuxArm64)
+            buildTargets += linuxArm64
+        }
+
         if (forMac) {
             def minMacOsVersion = '10.15'
             def mac64 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.MacOsX, true)
@@ -128,6 +135,8 @@ class GenerateLibs extends DefaultTask {
             BuildExecutor.executeAnt(jniDir + '/build-windows64.xml', commonParams)
         if (forLinux)
             BuildExecutor.executeAnt(jniDir + '/build-linux64.xml', commonParams)
+        if (forLinuxArm64)
+            BuildExecutor.executeAnt(jniDir + '/build-linuxarm64.xml', commonParams)
         if (forMac)
             BuildExecutor.executeAnt(jniDir + '/build-macosx64.xml', commonParams)
         if (forMacArm64)
